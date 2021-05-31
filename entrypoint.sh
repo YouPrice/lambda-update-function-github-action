9e41fb1edb9945219b8ee251c91d9046d1485bbb
Riccardo Miccini <miccio-dk@users.noreply.github.com>
2021-05-31 22:19:24 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,11 +11,12 @@ aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
 # comando per aggiornare layer 
-# quello già usato 
-# aws lambda publish-layer-version --layer-name my-layer --description "My layer" --license-info "MIT" --content S3Bucket=youprice-scraper-bucket,S3Key=layer.zip --compatible-runtimes python3.6 python3.7 python3.8 python3.9
-
-aws s3 cp ${FILE} s3://${S3_BUCKET}/${S3_KEY} \
-  --region ${AWS_REGION} $*
+aws lambda publish-layer-version \
+  --layer-name ${LAYER_NAME} \
+  --description "Lambda Layer" \
+  --license-info "MIT" \
+  --content S3Bucket=${S3_BUCKET},S3Key=${S3_KEY} \
+  --compatible-runtimes python3.6 python3.7 python3.8 python3.9
 
 rm -rf ~/.aws
 
