af64cea191bd412276ee631b761c1a8e03c721a6
edofra <edoardofrapiccini@alice.it>
2021-05-31 22:12:42 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,28 +2,6 @@
 
 set -e
 
-# if [ -z "$S3_BUCKET" ]; then
-#   echo "S3_BUCKET is not set. Quitting."
-#   exit 1
-# fi
-# if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-#   echo "AWS_ACCESS_KEY_ID is not set. Quitting."
-#   exit 1
-# fi
-# if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-#   echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
-#   exit 1
-# fi
-
-# if [-z "$FILE"]; then
-#   echo "FILE is not set. Quitting"
-#   exit 1
-# fi
-
-# if [ -z "$AWS_REGION"]; then
-#   AWS_REGION="us-east-1"
-# fi
-
 mkdir -p ~/.aws
 
 touch ~/.aws/credentials
@@ -31,6 +9,10 @@ touch ~/.aws/credentials
 echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
+
+# comando per aggiornare layer 
+# quello già usato 
+# aws lambda publish-layer-version --layer-name my-layer --description "My layer" --license-info "MIT" --content S3Bucket=youprice-scraper-bucket,S3Key=layer.zip --compatible-runtimes python3.6 python3.7 python3.8 python3.9
 
 aws s3 cp ${FILE} s3://${S3_BUCKET}/${S3_KEY} \
   --region ${AWS_REGION} $*
