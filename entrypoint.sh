3b4578ec2ec4721fee91073cec9e0811b656cb75
Riccardo Miccini <miccio-dk@users.noreply.github.com>
2021-06-01 16:59:30 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,6 @@ aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 # comando per aggiornare layer 
 aws lambda publish-layer-version \
   --layer-name ${LAYER_NAME} \
-  --description "Lambda Layer" \
   --license-info "MIT" \
   --content S3Bucket=${S3_BUCKET},S3Key=${S3_KEY} \
   --compatible-runtimes python3.6 python3.7 python3.8 python3.9
