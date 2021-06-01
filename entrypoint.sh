972988991c8dd07e1670f77b1da131941d51fecd
Riccardo Miccini <miccio-dk@users.noreply.github.com>
2021-06-01 17:46:43 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,12 +11,14 @@ aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
 # comando per aggiornare layer 
-aws lambda publish-layer-version \
+LAYER_ARN = $(aws lambda publish-layer-version \
   --layer-name ${LAYER_NAME} \
   --license-info "MIT" \
   --content S3Bucket=${S3_BUCKET},S3Key=${S3_KEY} \
   --region ${AWS_REGION} \
-  --compatible-runtimes python3.6 python3.7 python3.8 python3.9
+  --compatible-runtimes python3.6 python3.7 python3.8 python3.9 | jq -r '.LayerVersionArn')
+  
+echo "AWS_LAYER_ARN=$LAYER_ARN" >> $GITHUB_ENV
 
 rm -rf ~/.aws
 
