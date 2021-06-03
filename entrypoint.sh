8defaae05d9aef0f5554950f95ef86cac4a13b82
edofra <edoardofrapiccini@alice.it>
2021-06-03 23:16:05 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,18 +10,13 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-echo ${LAYER_NAME}
-echo ${S3_BUCKET}
-echo ${S3_KEY}
-echo ${AWS_REGION}
-
 # comando per aggiornare layer 
 LAYER_ARN=$(aws lambda publish-layer-version \
   --layer-name ${LAYER_NAME} \
   --license-info "MIT" \
   --content S3Bucket=${S3_BUCKET},S3Key=${S3_KEY} \
   --region ${AWS_REGION} \
-  --compatible-runtimes python3.6 python3.7 python3.8 python3.9)
+  --compatible-runtimes python3.6 python3.7 python3.8 python3.9 | jq -r '.LayerVersionArn')
   
 echo "AWS_LAYER_ARN=$LAYER_ARN" >> $GITHUB_ENV
 
