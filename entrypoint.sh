80a78fbf3fa38c16717ba5094649292fbc26e1aa
edofra <edoardofrapiccini@alice.it>
2021-06-09 19:02:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 # comando per aggiornare la funzione lambda
 aws lambda update-function-code \
   --function-name ${AWS_LAMBDA_NAME} \
-  --zip-file ${ZIP_FILE} \
+  --zip-file fileb://${ZIP_FILE} \
   --region ${AWS_REGION}
   
 echo "AWS_LAYER_ARN=$LAYER_ARN" >> $GITHUB_ENV
